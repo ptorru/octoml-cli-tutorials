734fe8aab9eccfa26eabee752b0bcfba63138823
Spencer Krum <nibz@spencerkrum.com>
2022-05-17 13:07:30 -0500

    feat: add example using http
    Before this change all our examples assumed grpc

--- tutorials/vision/run.py
+++ tutorials/vision/run.py
@@ -1,7 +1,9 @@
 import argparse
 import numpy as np
 from PIL import Image
+from tritonclient.utils import InferenceServerException
 import tritonclient.grpc as grpc_client
+import tritonclient.http as httpclient
 from ariel import function_from_model
 
 # onnxruntime doesn't support python 3.10
@@ -60,12 +62,40 @@ def run_triton(port, hostname="localhost"):
     # Interpret predictions
     interpret_cat_scores(pred)
 
+def run_triton_http(port, hostname="localhost"):
+    # Prepare client
+    url = "%s:%s" % (hostname, port)
+    triton_client = httpclient.InferenceServerClient(url=url)
+
+    # Preprocess input image
+    image = Image.open("cat_input_images/prey1.jpeg")
+    processed = image_preprocess([image])
+    img = processed[0]
+
+
+    # Initialize the model
+    inputs = []
+    # can look up inputs/outputs via:
+    # curl http://localhost:8000/v2/models/critterblock/config
+    inputs.append(httpclient.InferInput('image', [1,3,224,224], "FP32"))
+    inputs[0].set_data_from_numpy(img, binary_data=False)
+
+    # Run inference
+    results = triton_client.infer("critterblock", inputs)
+
+
+    # Interpret predictions
+    pred = [results.as_numpy('cat_image_type')]
+    interpret_cat_scores(pred)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Choose mode to run inference in.')
     parser.add_argument("--local", default=False, action="store_true")
     parser.add_argument("--triton", default=False, action="store_true")
     parser.add_argument("--hostname", default="localhost")
+    parser.add_argument("--protocol", default="grpc")
     parser.add_argument("--port", default=8001)
     args = parser.parse_args()
 
@@ -73,4 +103,16 @@ if __name__ == "__main__":
         run_local()
 
     if args.triton:
-        run_triton(args.port, args.hostname)
+        if args.protocol == "grpc":
+            run_triton(args.port, args.hostname)
+        elif args.protocol == "http":
+            # assume http to imply port 8000
+            if args.port == 8001:
+                http_port = 8000
+            else:
+                http_port = args.port
+            run_triton_http(http_port, args.hostname)
+        else:
+            print("Unknown protocol given. Supported protocols are grpc and http")
+
+
