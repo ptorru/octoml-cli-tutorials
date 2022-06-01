a0d929df26d16ba619fa14f41c5eae70434ec813
iansawicki <iswick@gmail.com>
2022-06-01 09:32:50 -0700

    Revert "Tweaks to make QA demo work. Added --QA arg to support dynamic input."
    This reverts commit c1ba28e8e8ffd49f626860bf0912a1ef990dc7eb.

--- tutorials/setup.sh
+++ tutorials/setup.sh
@@ -16,7 +16,7 @@ pip install -r requirements.txt
 
 
 # Get the bert model for the question answering demo
-${PYTHON} -m transformers.onnx --model=bert-large-uncased-whole-word-masking-finetuned-squad --feature=question-answering question_answering
+${PYTHON} -m transformers.onnx --model=bert-large-uncased-whole-word-masking-finetuned-squad --feature=question-answering qa
 
 # Get the gpt2 model for the generation model
 curl -fsSL -o generation/gpt2-lm-head-10.onnx https://github.com/onnx/models/raw/main/text/machine_comprehension/gpt-2/model/gpt2-lm-head-10.onnx
