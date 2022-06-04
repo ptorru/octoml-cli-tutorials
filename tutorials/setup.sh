ee3ba1319b934b20e3f76dcda3c6e398d8ea88a9
Spencer Krum <nibz@spencerkrum.com>
2022-06-03 23:36:08 -0500

    Use ariel from pip

--- tutorials/setup.sh
+++ tutorials/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eux
 
-# Get setup dir
-SETUP_DIR=$(dirname "$0")
-
 PYTHON=python
 # Compat for python3
 if ! command -v python &> /dev/null
@@ -16,13 +13,10 @@ pip install -r requirements.txt
 
 
 # Get the bert model for the question answering demo
-${PYTHON} -m transformers.onnx --model=bert-large-uncased-whole-word-masking-finetuned-squad --feature=question-answering qa
+${PYTHON} -m transformers.onnx --model=bert-large-uncased-whole-word-masking-finetuned-squad --feature=question-answering question_answering
 
 # Get the gpt2 model for the generation model
 curl -fsSL -o generation/gpt2-lm-head-10.onnx https://github.com/onnx/models/raw/main/text/machine_comprehension/gpt-2/model/gpt2-lm-head-10.onnx
-
-# Install ariel lib
-pip install "${SETUP_DIR}"/../ariel
 
 # Check for docker installation
 if ! command -v docker &> /dev/null
